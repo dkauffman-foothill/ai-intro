809084646f3e4f98632e906abd1b9e1f80e6138d
Daniel Kauffman <kauffmandaniel@fhda.edu>
2025-09-27 14:31:23 -0700

    Initial commit

---
+++ heuristic-search/heuristic.py
@@ -0,0 +1,77 @@
+class Heuristic:
+
+    @staticmethod
+    def get(tiles: tuple[int, ...]) -> int:
+        """
+        Return the estimated distance to the goal using Manhattan distance
+        and linear conflicts.
+
+        Only this static method should be called during a search; all other
+        methods in this class should be considered private.
+
+        >>> Heuristic.get((0, 1, 2, 3))
+        0
+        >>> Heuristic.get((3, 2, 1, 0))
+        6
+        """
+        width = int(len(tiles) ** 0.5)
+        return (Heuristic._get_manhattan_distance(tiles, width)
+                + Heuristic._get_linear_conflicts(tiles, width))
+
+    @staticmethod
+    def _get_linear_conflicts(tiles: tuple[int, ...], width: int) -> int:
+        """
+        Return the number of linear conflicts in the tiles, which represents
+        the minimum number of tiles in each row and column that must leave and
+        re-enter that row or column in order for the puzzle to be solved.
+        """
+        conflicts = 0
+        rows = [[] for i in range(width)]
+        cols = [[] for i in range(width)]
+        for i in range(len(tiles)):
+            if tiles[i] != 0:
+                if i // width == tiles[i] // width:
+                    rows[i // width].append(tiles[i])
+                if i % width == tiles[i] % width:
+                    cols[i % width].append(tiles[i])
+        for i in range(width):
+            conflicts += Heuristic._count_conflicts(rows[i])
+            conflicts += Heuristic._count_conflicts(cols[i])
+        return conflicts * 2
+
+    @staticmethod
+    def _count_conflicts(ints: list[int]) -> int:
+        """
+        Return the minimum number of tiles that must be removed from the given
+        list in order for the list to be sorted.
+        """
+        if Heuristic._is_sorted(ints):
+            return 0
+        lowest = None
+        for i in range(len(ints)):
+            conflicts = Heuristic._count_conflicts(ints[:i] + ints[i + 1:])
+            if lowest is None or conflicts < lowest:
+                lowest = conflicts
+        return 1 + lowest
+
+    @staticmethod
+    def _is_sorted(ints: list[int]) -> bool:
+        """Return True if the given list is sorted and False otherwise."""
+        for i in range(len(ints) - 1):
+            if ints[i] > ints[i + 1]:
+                return False
+        return True
+
+    @staticmethod
+    def _get_manhattan_distance(tiles: tuple[int, ...], width: int) -> int:
+        """
+        Return the Manhattan distance of the given tiles, which represents
+        how many moves is tile is away from its goal position.
+        """
+        distance = 0
+        for i in range(len(tiles)):
+            if tiles[i] != 0:
+                row_dist = abs(i // width - tiles[i] // width)
+                col_dist = abs(i % width - tiles[i] % width)
+                distance += row_dist + col_dist
+        return distance
---
+++ heuristic-search/puzzle.py
@@ -0,0 +1,126 @@
+import heuristic
+
+
+def is_optimal_solution(tiles: tuple[int], path: str, opt_len: int) -> bool:
+    return is_solution(tiles, path) and len(path) <= opt_len
+
+
+def is_solution(tiles: tuple[int], path: str) -> bool:
+    if not isinstance(path, str):
+        return False
+    state = State(tiles, "")
+    for move in path:
+        try:
+            state = state._move_tile(move)
+        except IndexError:
+            return False
+    return state.tiles == tuple(sorted(tiles))
+
+
+class State:
+
+    _MOVES = "←↑↓→"
+
+    def __init__(self, tiles: tuple[int, ...], path: str) -> None:
+        self.tiles = tuple(tiles)
+        self.path = path
+        self.h = heuristic.Heuristic.get(tiles)
+        self._size = int(len(tiles) ** 0.5)
+        self._cost = len(self.path) + self.h
+
+    def __lt__(self, other) -> bool:
+        return self._cost < other._cost
+
+    def create_new_states(self) -> list["State"]:
+        states = []
+        for move in State._MOVES:
+            if (
+                self._is_valid_move(move)
+                and not self._is_opposing_move(move)
+            ):
+                states.append(self._move_tile(move))
+        return states
+
+    def is_solvable(self) -> bool:
+        """
+        Return True if the given tiles represent a solvable puzzle and False
+        otherwise.
+
+        >>> is_solvable((3, 2, 1, 0))
+        True
+        >>> is_solvable((0, 2, 1, 3))
+        False
+        """
+        _, inversions = self._count_inversions(list(self.tiles))
+        width = int(len(self.tiles) ** 0.5)
+        if width % 2 == 0:
+            row = self.tiles.index(0) // width
+            return (
+                row % 2 == 0 and inversions % 2 == 0 or
+                row % 2 == 1 and inversions % 2 == 1
+            )
+        else:
+            return inversions % 2 == 0
+
+    def _is_valid_move(self, move: str) -> bool:
+        i = self.tiles.index(0)
+        if move == State._MOVES[0]:
+            return (i + 1) % self._size != 0
+        elif move == State._MOVES[1]:
+            return i // self._size != 0
+        elif move == State._MOVES[2]:
+            return (i // self._size) + 1 < self._size
+        elif move == State._MOVES[3]:
+            return i % self._size != 0
+        else:
+            return False
+
+    def _is_opposing_move(self, move: str) -> bool:
+            if len(self.path) == 0:
+                return False
+            opposing_move = State._MOVES[::-1].find(move)
+            return State._MOVES.find(self.path[-1]) == opposing_move
+
+    def _move_tile(self, move: str) -> "State":
+        """
+        Given tiles and a move to apply, return the updated tiles.
+        """
+        tiles = list(self.tiles)
+        i = tiles.index(0)
+        if move.upper() == State._MOVES[0]:
+            tiles[i], tiles[i + 1] = tiles[i + 1], tiles[i]
+        elif move.upper() == State._MOVES[1]:
+            tiles[i], tiles[i - self._size] = tiles[i - self._size], tiles[i]
+        elif move.upper() == State._MOVES[2]:
+            tiles[i], tiles[i + self._size] = tiles[i + self._size], tiles[i]
+        elif move.upper() == State._MOVES[3]:
+            tiles[i], tiles[i - 1] = tiles[i - 1], tiles[i]
+        return State(tuple(tiles), self.path + move)
+
+    def _count_inversions(self, ints: list[int]) -> tuple[list[int], int]:
+        """
+        Count the number of inversions in the given sequence of integers (ignoring zero), and return the sorted sequence along with the inversion count.
+
+        This function is only intended to assist `is_solvable`.
+
+        >>> _count_inversions([3, 7, 1, 4, 0, 2, 6, 8, 5])
+        ([1, 2, 3, 4, 5, 6, 7, 8], 10)
+        """
+        if len(ints) <= 1:
+            return ([], 0) if 0 in ints else (ints, 0)
+        midpoint = len(ints) // 2
+        l_side, l_inv = self._count_inversions(ints[:midpoint])
+        r_side, r_inv = self._count_inversions(ints[midpoint:])
+        inversions = l_inv + r_inv
+        i = j = 0
+        sorted_tiles = []
+        while i < len(l_side) and j < len(r_side):
+            if l_side[i] <= r_side[j]:
+                sorted_tiles.append(l_side[i])
+                i += 1
+            else:
+                sorted_tiles.append(r_side[j])
+                inversions += len(l_side[i:])
+                j += 1
+        sorted_tiles += l_side[i:] + r_side[j:]
+        return (sorted_tiles, inversions)
